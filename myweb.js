b492c24ad1d5a418744efb3bbea0a4d61ae69a8f
salman371k <salman371k@gmail.com>
2024-02-06 20:51:25 +0530

    change

--- myweb.js
+++ myweb.js
@@ -1,21 +1,39 @@
-function sendemail(){
-    var params={
-        name:document.getElementById("name").value ,
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value, 
-    };
-
-    const serviceID="service_p4uvl2a";
-    const templateID="template_2jr2k6t";
+// function sendemail(){
+//     var params={
+//         name:document.getElementById("name").value ,
+//         email:document.getElementById("email").value,
+//         message:document.getElementById("message").value, 
+//     };
+//     const serviceID=;
+//     const templateID=;
     
-    emailjs
-    .send(serviceID , templateID, params)
-    .then(res=>{
-        document.getElementById("name").value=" ";
-        document.getElementById("email").value=" ";
-        document.getElementById("message").value=" ";
-        console.log(res);
-        alert("Send message")
-    })
-    .catch((err)=>console.log(err));
-};
+//     emailjs
+//     .send(serviceID , templateID, params)
+//     .then(res=>{
+//         document.getElementById("name").value=" ";
+//         document.getElementById("email").value=" ";
+//         document.getElementById("message").value=" ";
+//         console.log(res);
+//         alert("Send message")
+//     })
+//     .catch((err)=>console.log(err));
+
+// function sendemail() {
+//     var params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         message: document.getElementById("message").value,
+//     };
+//     const serviceID = "service_p4uvl2a";
+//     const templateID = "template_2jr2k6t";
+    
+//     emailjs.send(serviceID, templateID, params)
+//         .then(res => {
+//             document.getElementById("name").value = "";
+//             document.getElementById("email").value = "";
+//             document.getElementById("message").value = "";
+//             console.log(res);
+//             alert("Message sent successfully!");
+//         })
+//         .catch(err => console.error("Error sending email:", err));
+// }
